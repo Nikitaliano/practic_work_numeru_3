a64be1d5cbc7562085afca9480fd185c7bb71cd1
Student <Student@B2-411-11>
2024-05-04 10:33:31 +0400

    Task +

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double first;
+            double second;
+            double result = 0;
+            Console.WriteLine("введите 1 слагаемое: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите 2 слагаемое: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            int operation;
+            Console.WriteLine("введите оперцию: ");
+            Console.WriteLine("1 = + \n2 = - \n3 = * \n4 = /");
+            operation =Convert.ToInt32(Console.ReadLine());
+            switch(operation)
+            {
+                case 1:
+                    result = first + second;
+                    break;
+
+                    default: Console.WriteLine("нет такой операции");
+                    break;
+            }
+            Console.WriteLine("ответ = " + result);
+            Console.ReadKey();
+
+        }
+    }
+}
